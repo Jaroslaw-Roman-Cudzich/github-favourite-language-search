d6897dfc859d3c5df8665d0f716b375653be3431
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-05-28 19:13:47 +0100

    Created a 'Results' file for the creation of the search results display field on the web page.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import SearchBar from "./components/SearchBar";
+import Results from "./components/Results";
 
 function App() {
   return <div>
-    <SeachBar />
+    <SearchBar />
     <Results />
     </div>;
 }
