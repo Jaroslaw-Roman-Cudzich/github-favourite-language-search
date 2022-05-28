7ba23aa521806bbca25ea61e6a6b247e68122f3e
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-05-28 23:00:07 +0100

    Added code to catch error when calling GitHub user's account data through GitHub API.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,9 @@
-import axios from "axios";
 import React, { useState } from "react";
 import axios from "axios";
 
 const SearchBar = () => {
     const [searchInput, setSearchInput] = useState('');
+    const [repos, setRepos] = useState([]);
 
     const handleChange = (e) => {
         setSearchInput(e.target.value);
@@ -12,10 +12,16 @@ const SearchBar = () => {
     const handleClick = async () => {
         console.log(searchInput);
 
-        const result = await axios(``)
+    try {
+        const result = await axios(`https://api.github.com/users/${searchInput}`);
 
+        setRepos(result);
+        } catch (err) {
+            console.log(err)
+        }
     };
 
+    console.log(repos);
     return (
         <div style={{padding: "20px"}}>
             <input type="text" 
