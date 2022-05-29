7252042397992ec52cf3c909c7d228f43a576a17
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-05-29 21:41:35 +0100

    Enabled the application to list the favourite (most frequently used) programming language from the searched GitHub user's account.

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,16 +2,25 @@ import React from "react";
 
 const Results = (props) => {
     const { repos } = props;
-    console.log("Repos is: ", repos.data);
+    console.log("GitHub user's repos: ", repos.data);
 
-    const listReposLanguages = 
-        repos.length !== 0 ? ( 
-            repos.data.map((item) => <li key={item.id}>{item.language}</li>)
+    const mostFreqLanguage = 
+        repos.length === 0 ? ( 
+ 
+                // repos.data.map((item) => <li key={item.id}>{item.language}</li>)
+
+            <li>Please enter a valid GitHub username as this one does not exist.</li>      
         ) : (
-            <li>Please enter a valid GitHub username as this one does not exist.</li>
-            );
+            (repos.data.map(function(el) {
+                return el.language;
+              })).reduce((previousLanguage, currentLanguage, currentIndex, arr) =>
+              arr.filter(language => language === previousLanguage).length >
+              arr.filter(language => language === currentLanguage).length
+                ? previousLanguage
+                : currentLanguage
+            ));
 
-    return <ul>{listReposLanguages}</ul>;
+    return <ul>{mostFreqLanguage}</ul>;
 };
 
 export default Results;
