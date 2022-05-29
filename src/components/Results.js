ba928120da098993648a4a4a936dcc573b662c67
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-05-29 18:09:32 +0100

    Enabled the application to list all the programming languages used in the searched GitHub user's account.

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,14 +4,14 @@ const Results = (props) => {
     const { repos } = props;
     console.log("Repos is: ", repos.data);
 
-    const listRepos = 
+    const listReposLanguages = 
         repos.length !== 0 ? ( 
-            repos.data.map((item) => <li key={item.id}>{item.name}</li>)
+            repos.data.map((item) => <li key={item.id}>{item.language}</li>)
         ) : (
             <li>Please enter a valid GitHub username as this one does not exist.</li>
             );
 
-    return <ul>{listRepos}</ul>;
+    return <ul>{listReposLanguages}</ul>;
 };
 
 export default Results;
